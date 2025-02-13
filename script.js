30e2f5c9844470741ea8349bd59bd96c41d26859
Aleks Vits <84374296+sozdatelsaitov@users.noreply.github.com>
2025-02-13 16:59:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const header = document.querySelector(".header");
+const burger = header.querySelector(".burger-menu");
+const burgerIcon = header.querySelector(".burger-menu_icon");
+
+burger.addEventListener("click", function(){
+  header.classList.toggle("header--mobile");
+
+  if(header.classList.contains("header--mobile")){
+    burgerIcon.src = "images/close.svg";
+  } else {
+    burgerIcon.src = "images/burger.svg";
+  }
+});
