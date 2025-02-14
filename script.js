8943b8164c20134018d542f96e011eecd57ff402
Aleks Vits <84374296+sozdatelsaitov@users.noreply.github.com>
2025-02-14 17:15:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -54,3 +54,48 @@ scrollRightButton.addEventListener('click', () => {
     behavior: 'smooth'
   });
 });
+
+
+
+
+// Извлечение первого кадра видео
+const video1 = document.getElementById('video1');
+const canvas1 = document.getElementById('canvas1');
+const context1 = canvas1.getContext('2d');
+
+video1.addEventListener('loadeddata', () => {
+  video1.currentTime = 1; // Перемотка на первую секунду
+});
+
+video1.addEventListener('seeked', () => {
+  context1.drawImage(video1, 0, 0, canvas1.width, canvas1.height);
+  const posterDataUrl = canvas1.toDataURL('image/jpeg');
+  video1.setAttribute('poster', posterDataUrl);
+});
+
+
+
+// Тёмные цвета
+document.addEventListener("DOMContentLoaded", function() {
+  const textBlocks = document.querySelectorAll('.visual__text-block');
+  const colors = ['#343a40', '#495057', '#212529', '#6c757d', '#000000'];
+
+  textBlocks.forEach(block => {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    block.style.backgroundColor = randomColor;
+  });
+});
+
+
+// светлые цвета
+// document.addEventListener("DOMContentLoaded", function() {
+//   const textBlocks = document.querySelectorAll('.visual__text-block');
+//   const colors = ['#a8dadc', '#457b9d', '#1d3557', '#e63946', '#f1faee'];
+
+//   textBlocks.forEach(block => {
+//     const randomColor = colors[Math.floor(Math.random() * colors.length)];
+//     block.style.backgroundColor = randomColor;
+//   });
+// });
+
+  
